35c1b0c8c14571187f0284669c136b5e36e675bc
Otavio Silva <otavioolsilva@gmail.com>
2020-05-05 10:30:15 -0300

    Incluida novas mensagens

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello world!!!" << endl;
+    cout << "Otavio Silva - Aula Git 01" << endl << "Professor Nelio Alves" << endl << "2CG IFTM Udi Centro" << endl;
     return 0;
 }
